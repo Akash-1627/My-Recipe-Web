e4b4bd7c56858692d0f1e94e8f9d876846e15f11
Akash1627 <akash1627.be21@chitkara.edu.in>
2022-09-18 14:13:04 +0530

    all files uploaded

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+let search = document.getElementById('search');
+let featured = document.getElementById('featured');
+let container = document.getElementById('notes');
+let nodata = document.getElementById('nodata');
+let bool = false;
+
+search.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        let searchval = search.value.toLowerCase();
+        let card = document.getElementsByClassName("card");
+        e.preventDefault();
+        Array.from(card).forEach(function(element){
+            let title = element.getElementsByTagName("h5")[0].innerText.toLowerCase();
+
+            if(title.includes(searchval)){
+                nodata.style.display = "none";
+                element.style.display = "block";
+                bool = true;
+            }
+            else{
+                element.style.display = "none";
+                
+            }
+        })
+        if(bool){
+            if(search.value!=""){
+
+                featured.innerHTML = search.value;
+            }
+            else{
+                featured.innerHTML = "Some Featured Recipes";
+                nodata.style.display = "none";
+                window.location.reload();
+            }
+        }
+        else{
+            featured.innerHTML = "Nothing matches your search";
+            nodata.style.display = "block";
+            
+        }
+        bool = false;
+
+        
+    }
+
+})
